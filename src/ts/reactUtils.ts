78820436ea2b1987b1da3b5fcc46119003f79d33
TobinPalmer <tobinkpalmer@gmail.com>
2024-02-21 18:21:01 -0800

    fixed reactUtils

--- src/ts/reactUtils.ts
+++ src/ts/reactUtils.ts
@@ -3,15 +3,10 @@ import {ReactPropsOuterWrapper} from "./types/duolingo";
 // This kindof recursivly looks up a subtree of elements and looks for something called __reactProps$RANDOMSTRING.
 // Then we basically go into the secret duolingo stuff that has the answer and return it.
 // The props contain a correct answer and a list of tokens that are the words in the answer.
-export function findReact(dom: Element, traverseUp = 0): ReactPropsOuterWrapper | undefined {
-    let reactProps = Object.keys(dom.parentElement as HTMLElement).find((key) => key.startsWith("__reactProps$")) ?? ""
+export function findReact(dom: Element): ReactPropsOuterWrapper | undefined {
+    let reactProps = Object.keys(dom as HTMLElement).find((key) => key.startsWith("__reactProps$")) ?? ""
 
-    while (traverseUp-- > 0 && dom.parentElement) {
-        dom = dom.parentElement;
-        reactProps = Object.keys(dom.parentElement as HTMLElement).find((key) => key.startsWith("__reactProps$")) ?? ""
-    }
-
-    const props = dom.parentElement?.[reactProps as keyof HTMLElement]
+    const props = dom?.[reactProps as keyof Element]
     if (isProps(props)) return props.children[0]._owner.stateNode.props
 }
 
